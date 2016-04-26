ca0a153f08cfa6d38585d6e974f77ae2cda7c810
madeleine789 <madeleine789@gmail.com>
2016-04-26 19:47:31 +0200

    Added conversion between EUR and PLN

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -3,8 +3,9 @@ import scraper.{OriginScraper, Scraper, SteamScraper}
 object App extends App {
   override def main(args: Array[String]): Unit = {
     var scraper : Scraper = new SteamScraper
-    println(scraper.getInfo("gta").head)
+    scraper.getInfo("counter-strike").foreach {println}
     scraper = new OriginScraper
-    println(scraper.getInfo("star wars").head)
+    println()
+    scraper.getInfo("star wars").foreach {println}
   }
 }
--- src/main/scala/game/GameInfo.scala
+++ src/main/scala/game/GameInfo.scala
@@ -1,14 +1,17 @@
 package game
 
+import java.time.LocalDateTime
+
+import scala.math.BigDecimal.RoundingMode
 import scala.util.parsing.json.JSON
 import scalaj.http._
 
-class GameInfo(_name: String, _price: String, _url: String) {
+case class GameInfo(_name: String, _price: String, _url: String) {
   val name: String  = _name
-  val price: String = _price
+  val price: PriceEntry = priceStrToPriceEntry(_price)
   val url: String = if (_url.contains("steam")) _url else shortenUrl(_url)
 
-  override def toString = { name + " [ " + price + " | " + url + " ]" }
+  override def toString = { name + " [ " + price.price.value + " " + price.price.currency + " | " + url + " ]" }
 
   def shortenUrl(_url: String = url) : String = {
     val apiKey = "R_e29aa9c6e2144e19a52df028f146c899"
@@ -21,4 +24,19 @@ class GameInfo(_name: String, _price: String, _url: String) {
       case None => _url
     }
   }
+
+  def priceStrToPriceEntry(_price: String, euroToPln: Double = 4.4) : PriceEntry = {
+    val pattern = """(\d+,*\d*).*""".r
+    var price = "0.00"
+    pattern.findAllIn(_price).matchData foreach {
+      m => {
+        price = m.group(1).replace(",", ".")
+        if (!_price.contains("zł")) price = (price.toDouble * euroToPln).toString;
+      };
+    }
+    new PriceEntry(LocalDateTime.now(), new Money(BigDecimal(price).setScale(2, RoundingMode.HALF_UP), "PLN"))
+  }
 }
+
+case class PriceEntry(date: LocalDateTime, price: Money)
+case class Money(value: BigDecimal, currency: String)
